25b0694e915207d23c68a83187e4a66f3af28914
Oksana Moroz <k.moroz.mail@gmail.com>
2023-10-12 17:09:00 +0300

    added order information

--- components/CartContext.js
+++ components/CartContext.js
@@ -23,9 +23,18 @@ export function CartContextProvider({ children }) {
         setCartProducts(prev => [...prev, productId]);
     }
 
+  function removeProduct(productId) {
+    setCartProducts(prev => {
+      const pos = prev.indexOf(productId)
+      if (pos !== -1) {
+       return prev.filter((value,index) => index !== pos)
+      }
+      return prev;
+    })
+  }
     return (
       <CartContext.Provider
-        value={{ cartProducts, setCartProducts, addProduct }}
+        value={{ cartProducts, setCartProducts, addProduct,removeProduct }}
       >
         {children}
       </CartContext.Provider>
---
+++ pages/api/checkout.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.json("should be a POST request");
+    return;
+  }
+  const { name, email, city, postalCode, streetAddress, country, products } =
+    req.body;
+}
+await mongooseConnect();
+const productsId = products.split(",");
+const uniqueIds = [...new Set(productsIds)];
+const productsInfos = await Product.find({ _id: uniqueIds });
+
+let line_items = [];
+for (const productId of uniqueIds) {
+  const productInfo = productsInfos.find((p) => p._id.toString() === productId);
+  const quantity = productsIds.filter((id) => id === productId)?.length || 0;
+  if (quantity > 0 && productInfo) {
+    line_items.push({
+      quantity,
+      price_data: {
+        currency: "USD",
+        product_data: { name: productInfo.title },
+        unit_amount: quantity * productInfo.price,
+      },
+    });
+  }
+}
+
+res.json({ line_items });
